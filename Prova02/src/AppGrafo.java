388b59b0fc243c252de316ed2bc5e365b64bb57b
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2023-06-02 16:56:15 -0300

    dfsfsdf

--- Prova02/src/AppGrafo.java
+++ Prova02/src/AppGrafo.java
@@ -11,13 +11,35 @@ public class AppGrafo {
         grafo.adicionarVertice("C");
         grafo.adicionarVertice("D");
         grafo.adicionarVertice("E");
+        grafo.adicionarVertice("F");
+        grafo.adicionarVertice("G");
+        grafo.adicionarVertice("H");
+        grafo.adicionarVertice("J");
+        grafo.adicionarVertice("K");
 
-        grafo.adicionarAresta(2.0,"A","B");
-        grafo.adicionarAresta(2.0,"B","A");
 
+        grafo.adicionarAresta(300.0,"A","B");
+        grafo.adicionarAresta(300.0,"B","A");
 
-        grafo.adicionarAresta(1.0,"C","D");
-        grafo.adicionarAresta(1.0,"D","C");
+        grafo.adicionarAresta(47.0,"B","C");
+        grafo.adicionarAresta(47.0,"C","B");
+
+        grafo.adicionarAresta(62.0,"C","D");
+        grafo.adicionarAresta(62.0,"D","C");
+
+        grafo.adicionarAresta(8.0,"D","E");
+        grafo.adicionarAresta(8.0,"E","D");
+
+        grafo.adicionarAresta(13.0,"E","F");
+        grafo.adicionarAresta(13.0,"F","E");
+
+        grafo.adicionarAresta(230.0,"E","G");
+        grafo.adicionarAresta(230.0,"G","E");
+
+        grafo.adicionarAresta(141.0,"C","H");
+        grafo.adicionarAresta(141.0,"H","C");
+
+        
 
 
         while(JOptionPane.showConfirmDialog(null,"Deseja ver uma rota?")!=1){
